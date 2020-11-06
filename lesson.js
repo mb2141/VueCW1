323f018e5b3afd48e399a06c9a583cb00261722f
mb2141 <mb2141@live.mdx.ac.uk>
2020-11-06 23:12:10 +0000

    Making a few adjustment before placing on github

---
+++ lesson.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        webname: "After school clubs",
+
+        subject: [{
+                id: 1,
+                image: "",
+                name: "English",
+                location: "Dartford",
+                price: 10.00,
+                space: 5,
+            },
+
+            {
+                id: 1,
+                image: "",
+                name: "Math",
+                location: "bexleyheath",
+                price: 10.00,
+                space: 5,
+            }
+        ],
+        cart: [],
+        ShowSubject: true,
+    },
+
+    methods: {
+        addToCart: function () {
+            this.cart.push(this.subject.id)
+        },
+        ShowBasket(){
+            this.ShowSubject = this.ShowSubject ? false : true;
+        }
+    },
+
+    computed: {
+        CartItemCount: function () {
+            return this.cart.length || '';
+        },
+        CanAddToCart: function () {
+            return this.subject[0].space > this.CartItemCount;
+            
+        }
+    }
+})
