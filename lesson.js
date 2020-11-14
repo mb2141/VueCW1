ecd13274eb8c2f2d68f570c5b0a385cec09ac351
mb2141 <mb2141@live.mdx.ac.uk>
2020-11-14 20:17:49 +0000

    changes

--- lesson.js
+++ lesson.js
@@ -3,88 +3,7 @@ var app = new Vue({
     data: {
         webname: "After school clubs",
 
-        subject: [{
-                id: 1,
-                image: "image/english.png",
-                name: "Foundation English",
-                location: "Dartford",
-                price: 1.50,
-                space: 5,
-            },
-
-            {
-                id: 2,
-                image: "image/math.png",
-                name: "Foundation Math",
-                location: "bexleyheath",
-                price: 1.50,
-                space: 5,
-            },
-            {
-                id: 3,
-                image: "image/science.png",
-                name: "Foundation Biology",
-                location: "bexleyheath",
-                price: 1.50,
-                space: 5,
-            },
-            {
-                id: 4,
-                image: "image/science.png",
-                name: "Foundation Physics",
-                location: "bexleyheath",
-                price: 1.50,
-                space: 5,
-            },
-            {
-                id: 5,
-                image: "image/science.png",
-                name: "Foundation Chemistry",
-                location: "bexleyheath",
-                price: 1.50,
-                space: 5,
-            },
-            {
-                id: 6,
-                image: "image/math.png",
-                name: "Higher Maths",
-                location: "Welling",
-                price: 2.00,
-                space: 5,
-            },
-            {
-                id: 7,
-                image: "image/english.png",
-                name: "Higher English",
-                location: "Dartford",
-                price: 2.00,
-                space: 5,
-            },
-            {
-                id: 8,
-                image: "image/ep.png",
-                name: "Ethics & Philosophy",
-                location: "Erith",
-                price: 2.00,
-                space: 5,
-            },
-            {
-                id: 9,
-                image: "image/business.png",
-                name: "Business studdies",
-                location: "Erith",
-                price: 2.00,
-                space: 5,
-            },
-            {
-                id: 10,
-                image: "image/computer.png",
-                name: "Computer Science",
-                location: "Erith",
-                price: 2.00,
-                space: 5,
-            },
-        ],
+        subject: subject,
 
         cart: [],
         ShowSubject: true,
@@ -95,8 +14,8 @@ var app = new Vue({
     },
 
     methods: {
-        addToCart: function () {
-            this.cart.push(this.subject.id)
+        addToCart: function (subject) {
+            this.cart.push(subject.id);
         },
         ShowBasket() {
             this.ShowSubject = this.ShowSubject ? false : true;
@@ -104,16 +23,24 @@ var app = new Vue({
 
         alert() {
             alert("Order Completed");
+        },
+
+        CanAddToCart(subject) {
+
+            return subject[1].space > this.CartCount(subject.id)
+        },
+
+        CartCount(subject) {
+            let count = 0;
+            for (let i=0; i<this.cart.length; i++){
+                if(this.cart[i] === subject.id) count++;
+            }
+            return count;
         }
     },
 
     computed: {
-        CartItemCount: function () {
-            return this.cart.length || '';
-        },
-        CanAddToCart: function () {
-            return this.subject[0].space > this.CartItemCount;
-        },
+ 
     }
 
 })
